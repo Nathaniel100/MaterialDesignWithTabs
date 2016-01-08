db37f4f5427297bd1820e16b078979ac3220cdef
wufan <386534960@qq.com>
2016-01-08 18:09:20 +0800

    Tabs with custom view

--- app/src/main/java/com/wufan/materialdesignwithtabs/MainActivity.java
+++ app/src/main/java/com/wufan/materialdesignwithtabs/MainActivity.java
@@ -8,8 +8,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,9 +56,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupIcons() {
-        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
-        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
-        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("ONE");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_action, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("TWO");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_photo, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("THREE");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_contacts, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -84,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return null;
+            return fragmentTitleList.get(position);
         }
     }
 }
